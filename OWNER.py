4fe63eda3702b54fb0e291028fd60786bdfff2f0
alnakhwdhhmhmd8 <alnakhwdhhmhmd8@gmail.com>
2025-08-10 23:17:58 +0300

    Uploaded Successfully

---
+++ OWNER.py
@@ -0,0 +1,11 @@
+# متطلبات التنصيب #
+OWNER = ["wwvvwl"]
+moord = 7728230165
+OWNER_NAME = "ᴊᴀᴋلـ™"
+BOT_TOKEN = "8490252476:AAE4BgPkVWENSnJGc6WRyZ852svWY9KRZTU"
+DATABASE = "mongodb+srv://mhmdalymany935:aHXo8qymo04IeJIg@jak.neur7n5.mongodb.net/?retryWrites=true&w=majority&appName=jak"
+CHANNEL = "https://t.me/qqvqqs/2551"
+GROUP = "https://t.me/xvwwvl"
+VIDEO = "https://files.catbox.moe/2b05ct.mp4"
+LOGS = "wwvvwl"
+
